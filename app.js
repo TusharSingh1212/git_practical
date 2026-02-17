da3ca26d777acb009de58a4e423d68889f8f5bd6
TusharSingh1212 <ts973121@gmail.com>
2026-02-17 14:15:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+let express=require('express');
+let bodyParser=require('body-parser');
+let app=express();
+
+let path=require('path');
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+app.get('/register',(req,res)=>{
+    res.render('index');
+});
+
+app.post('/register',(req,res)=>{
+    console.log(req.body);
+    let {upwd,ucpwd}=req.body;
+    if(upwd!==ucpwd){
+        return res.send('Enter Same Passwords');
+    }
+    else{
+        console.log('User Registered:',upwd);
+    }
+    res.send('User Registered Successfully');
+});
+
+app.get('/',(req,res)=>{
+    res.send(log.query);
+});
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+});
